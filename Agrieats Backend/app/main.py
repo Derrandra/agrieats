43f085ba9375f6f54f8c10bf15f75b0aedb76a6f
derr <andraasm1101@gmail.com>
2026-05-20 02:53:23 +0700

    api connect fe be
    a

--- Agrieats Backend/app/main.py
+++ Agrieats Backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.db.database import engine, Base
 from app.db import models 
 from app.api.routes import mahasiswa, auth, umkm, menu
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -9,6 +10,19 @@ app = FastAPI(
     title="AgriEats API",
     description="Backend untuk platform AgriEats IPB",
     version="1.0.0"
+)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],      
+    allow_headers=["*"],
 )
 
 app.include_router(mahasiswa.router, prefix="/api/mahasiswa", tags=["Mahasiswa"])
