2ec2ec1c3f323da5d49be5caf2caca42d0908c79
derr <andraasm1101@gmail.com>
2026-06-03 17:49:16 +0700

    db

--- Agrieats Backend/app/db/database.py
+++ Agrieats Backend/app/db/database.py
@@ -5,14 +5,19 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if SQLALCHEMY_DATABASE_URL:
+    if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql+psycopg://", 1)
+    elif SQLALCHEMY_DATABASE_URL.startswith("postgresql://"):
+        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-# Dependency untuk mendapatkan session DB di setiap request API
 def get_db():
     db = SessionLocal()
     try:
