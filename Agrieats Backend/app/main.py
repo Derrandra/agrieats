5b2a70cf1b4e78fc4e8a1d9a1e1a0c33928873ff
derr <andraasm1101@gmail.com>
2026-06-03 23:56:19 +0700

    Update main.py

--- Agrieats Backend/app/main.py
+++ Agrieats Backend/app/main.py
@@ -19,7 +19,7 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 origins = [
     "http://localhost:5173",
-    "https://agrieats-ipb.vercel.app",
+    "https://agrieats-vercel-qh3v.vercel.app/",
 ]
 
 app.add_middleware(
